2820b3fc5e24e8f24d5c07a4677c530be121b2de
Artem2690 <57846421+Artem2690@users.noreply.github.com>
2021-02-22 18:20:53 +0200

    Created a routing class

--- Core/base/controllers/RoutController.php
+++ Core/base/controllers/RoutController.php
@@ -7,8 +7,17 @@ namespace Core\base\controllers;
 class RoutController
 {
     static private $_instance;
+    private function  __construct(){
+
+    }
+    private function __clone(){
+
+    }
     static public function getInstance(){
-        return self::$_instance;
+        if(self::$_instance instanceof self){
+            return self::$_instance;
+        }
+        return self::$_instance = new self;
     }
 
 }
