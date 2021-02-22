bd8f6c5c86882239714543351511d2acb899703b
Artem2690 <57846421+Artem2690@users.noreply.github.com>
2021-02-22 20:52:34 +0200

    Created a settings class

--- Core/base/settings/Settings.php
+++ Core/base/settings/Settings.php
@@ -1,10 +1,50 @@
 <?php
-
+//файл настроек фреймворка
+//использован шаблон проектирования singleton
 
 namespace core\base\settings;
 
 
 class Settings
 {
+    static private $_instance;
+
+    private $routes =[
+      'admin'=>[
+          'name'=>'admin',//url для входа в админ панель
+          'path'=>'core/admin/controllers/',//пространство имен
+          'hrUrl'=> false //Понятный для человека url
+
+      ],
+      'settings'=> [
+           'path'=>'core/base/settings'//пространство имен
+
+      ],
+      'plugins'=>[
+          'path'=>'core/plugins',//пространство имен
+          'hrUrl'=> false //Понятный для человека url
+      ],
+      'user'=>[
+          'path'=>'core/user/controllers/',//пространство имен
+          'hrUrl'=> true, //Понятный для человека url
+          'routes'=>[
+
+              ]
+      ],
+      'default' => [
+          'controller'=> 'IndexController',//Контроллер по умолчанию
+          'inputMethod'=> 'InputData',//Метод по умолчанию сбора данних
+          'outputMethod'=> 'OutputData' //Метод по умолчанию вывода данних
+      ]
+    ];
+    //Защищаем от создания через new
+    private function __construct(){
+
+    }
+    //Защищаем от создания через клонировани
+    private function  __clone()
+    {
+
+    }
 
 }
